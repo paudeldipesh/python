d96920ed3b96ddacec594768be109609566f208a
Dipesh Paudel <thedeepyash7@gmail.com>
2024-04-17 18:38:19 +0545

    day 005

---
+++ 005-comments-and-print/index.py
@@ -0,0 +1,9 @@
+# This is a single-line comment.
+"""
+This is a multi-line comment.
+A computer is an electronic machine that can perform several tasks according to the given instructions.
+"""
+print('It\'s Dipesh Paudel.\nA \"good\" boy.')
+print("Namaste Nepal")  # Printing Namaste Nepal
+print("Word", 7, sep="_", end="_Haha\n")
+print("Shiva")
