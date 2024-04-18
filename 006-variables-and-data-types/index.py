6b1e85a9ffdb31da025f02f2f8500041d0555632
Dipesh Paudel <thedeepyash7@gmail.com>
2024-04-18 08:42:31 +0545

    day 006

---
+++ 006-variables-and-data-types/index.py
@@ -0,0 +1,20 @@
+a = "Hi"
+b = 12345
+c = 3.14
+d = True
+e = None
+f = complex(8, 2)
+list_one = [8, True, [-3, 7], ["apple","banana"]]
+tuple_one = (("parrot", "sparrow"), ("lion", "tiger"))
+dict_one = {"name": "Dipesh Paudel", "age": 23, "canVote": True}
+set_one = {1, 2, 3, 4, 5}
+print("Type of a", type(a))
+print("Type of b", type(b))
+print("Type of c", type(c))
+print("Type of d", type(d))
+print("Type of e", type(e))
+print("Type of f", type(f))
+print("Type of list_one", type(list_one))
+print("Type of tuple_one", type(tuple_one))
+print("Type of dict_one", type(dict_one))
+print("Type of set_one", type(set_one))
