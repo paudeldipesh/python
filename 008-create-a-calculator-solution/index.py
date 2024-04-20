b4481b72f0808b9a5db5a430222755c1b21dda54
Dipesh Paudel <thedeepyash7@gmail.com>
2024-04-20 18:41:33 +0545

    day 008

---
+++ 008-create-a-calculator-solution/index.py
@@ -0,0 +1,6 @@
+a = 50
+b = 3
+print("The value of", a, "+", b, "is", a + b)
+print("The value of " + str(a) + "-" + str(b) + " is " + str(a - b))
+print(f"The value of {a}/{b} is {a / b}")
+print("The value of {}*{} is {}".format(a, b, a * b))
