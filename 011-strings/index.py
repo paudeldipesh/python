4de39484a92e236489d3f30ea9680928b41e043f
Dipesh Paudel <thedeepyash7@gmail.com>
2024-04-23 21:21:41 +0545

    day 011

---
+++ 011-strings/index.py
@@ -0,0 +1,16 @@
+name = "Dipesh"
+print(name[0])
+print(name[5])
+
+for character in name:
+  print(character)
+
+print("Hello, " + name)
+
+apple = "He said, 'I want to eat an apple'."
+print(apple)
+
+computer = '''A computer is an electronic machine
+that can perform several tasks according
+to the given instructions.'''
+print(computer)
