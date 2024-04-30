c01ddfaccc42bb2be5071a94aa4318968c196f30
Dipesh Paudel <thedeepyash7@gmail.com>
2024-04-30 10:43:17 +0545

    day 018

---
+++ 018-while-loops/index.py
@@ -0,0 +1,12 @@
+for digit in range(5):
+  print(digit)
+
+number = 0
+while number < 7:
+  number = int(input("Enter a number: "))
+  print(number)
+
+figure = 6
+while figure > 0:
+  print(figure)
+  figure -= 1
