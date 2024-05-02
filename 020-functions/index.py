ddae116f05b857bc558b61b1493112e024384394
Dipesh Paudel <thedeepyash7@gmail.com>
2024-05-02 08:30:23 +0545

    day 020

---
+++ 020-functions/index.py
@@ -0,0 +1,16 @@
+first_digit = 4
+second_digit = 16
+average = (first_digit + second_digit) / 2
+print(average)
+
+
+def print_something():
+  pass
+
+
+def calculate_average(first, second, third):
+  return print((first + second + third) / 3)
+
+
+calculate_average(1, 2, 3)
+calculate_average(4, 5, 6)
