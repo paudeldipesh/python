2282591a21b4a9e3d3416e366823cfe84464a0a4
Dipesh Paudel <thedeepyash7@gmail.com>
2024-05-24 20:18:50 +0545

    day 044

---
+++ 044-how-import-works/dipesh.py
@@ -0,0 +1,5 @@
+def say_welcome(name: str):
+    print(f"Welcome, {name.title()}")
+
+
+dipesh = "Dipesh Paudel is a professional developer."
---
+++ 044-how-import-works/index.py
@@ -0,0 +1,11 @@
+import math as Mathematics
+
+from dipesh import *
+
+print(Mathematics.factorial(6))
+print(Mathematics.sqrt(4))
+print(dir(Mathematics))
+print(type(Mathematics.nan))
+
+print(dipesh)
+say_welcome("rajesh hamal")
