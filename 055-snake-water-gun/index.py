fb3ece66ab1cbf42b43cd91d9e07f36f56993f56
Dipesh Paudel <thedeepyash7@gmail.com>
2024-05-26 23:07:33 +0545

    day 055

---
+++ 055-snake-water-gun/index.py
@@ -0,0 +1,3 @@
+"""
+Snake, Water, Gun is a variation of the children's game rock-paper-scissors. Players use hand gestures to represent a snake, water, or a gun. The gun shoots the snake (gun wins), water extinguishes the gun (water wins), and the snake drinks the water (snake wins). Write a Python program to create a Snake Water Gun game using if-else statements. We will avoid fancy GUIs and use functions to determine the winner.
+"""
