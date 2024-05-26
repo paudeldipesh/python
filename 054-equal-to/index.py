854739901c4df9d86705d26dac3173a3c77ce50d
Dipesh Paudel <thedeepyash7@gmail.com>
2024-05-26 23:02:29 +0545

    day 054

---
+++ 054-equal-to/index.py
@@ -0,0 +1,7 @@
+value_one = "Dipesh Paudel"
+value_two = "Dipesh Paudel"
+
+# Compare values in programming
+print(value_one == value_two)
+# Exact location of object in memory is True for immutable values
+print(value_one is value_two)
