42f4b22e28a102d665e7925fae77c010df510594
Dipesh Paudel <thedeepyash7@gmail.com>
2024-05-31 14:22:22 +0545

    day 083

---
+++ 083-shoutouts-to-everyone/index.py
@@ -0,0 +1,14 @@
+"""
+Write a program to pronounce list of names using win32 API. If you are given a
+list as follows:
+
+list = ["Rahul", "Nishant", "Harry"]
+
+Your program should pronouce:
+
+Shoutout to Rahul
+Shoutout to Nishant
+Shoutout to Harry
+
+Note: If you are not using windows, try to figure out how to do the same thing using some other package.
+"""
