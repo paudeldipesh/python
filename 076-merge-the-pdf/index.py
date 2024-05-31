a68532270c07c926017fabf7ba7d7ea007a7d027
Dipesh Paudel <thedeepyash7@gmail.com>
2024-05-31 06:16:51 +0545

    day 076

---
+++ 076-merge-the-pdf/index.py
@@ -0,0 +1,5 @@
+"""
+Write a program to manipulate PDF files using PyPDF. Your program should be able to merge multiple PDF files into a single PDF. You are welcome to add more functionalities.
+
+PyPDF is a free and open-source pure-Python PDF library capable of splitting, merging, cropping, and transforming the pages of PDF files. It can also add custom data, viewing options, and passwords to PDF files. PyPDF can retrieve text and metadata from PDFs as well.
+"""
